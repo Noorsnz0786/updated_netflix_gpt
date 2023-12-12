04f349ab84ccce9853e812437a379dddf2a5da0d
Noorsnz0786 <65485029+Noorsnz0786@users.noreply.github.com>
2023-12-12 15:18:50 +0530

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-cyan-400 " >
+      Namaste everyone, Now i am building NETFLIX GPT
+    </div>
+  );
+}
+
+export default App;
